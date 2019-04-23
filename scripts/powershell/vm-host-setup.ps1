89e4b0dfdb0a961dfc5b35a84691a1c1a6fb2da3
Marcin Narloch <marcin.narloch@qbituniverse.com>
2019-04-23 22:16:37 +0100

    Added compose & mode switch

--- scripts/powershell/vm-host-setup.ps1
+++ scripts/powershell/vm-host-setup.ps1
@@ -6,6 +6,10 @@ Uninstall-Package -Name docker -ProviderName DockerMSFTProvider -Force
 Install-Module DockerProvider -Force
 Install-Package Docker -ProviderName DockerProvider -RequiredVersion preview -Force
 
+# add Docker Compose
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\Docker\docker-compose.exe
+
 # install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
