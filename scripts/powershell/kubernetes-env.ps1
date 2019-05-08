846bb507df31ca8bdc180c701a51ba862b114ff3
marcinnarlochbd <37069983+marcinnarlochbd@users.noreply.github.com>
2019-05-08 11:40:53 +0100

    Update kubernetes-env.ps1 (#18)

--- scripts/powershell/kubernetes-env.ps1
+++ scripts/powershell/kubernetes-env.ps1
@@ -1,150 +1,145 @@
 [CmdletBinding()]
 Param
 (
-	[Parameter(Position=0,Mandatory=$false,HelpMessage="Operation: --view | --view-wide")]
+	[Parameter(Position=0,Mandatory=$false,HelpMessage="Operation: --view | --reset | --pd | --pu | --pc | --pa")]
 	[string]$Operation
 )
 
 If ($Operation -in "--help") {
 	Write-Host -ForegroundColor Green ""
-	Write-Host -ForegroundColor Green "Kubectl Environment:"
+	Write-Host -ForegroundColor Green "Docker Environment:"
 	Write-Host -ForegroundColor Green ""
 	Write-Host -ForegroundColor Green "	[operation]"
 	Write-Host -ForegroundColor Green ""
-	Write-Host -ForegroundColor Green "	--view			View all k8s resources: all contexts, all namespaces, all deployments, all replica sets, all services, all pods."
-	Write-Host -ForegroundColor Green "	--view-wide		View all k8s resources in wide view: all contexts, all namespaces, all deployments, all replica sets, all services, all pods."
+	Write-Host -ForegroundColor Green "	--view			View all docker resources: system stats, all images, all containers, all volumes, dangling volumes, dangling images."
+	Write-Host -ForegroundColor Green "	--reset			Resets Docker networking adapter."
+	Write-Host -ForegroundColor Green "	--pd			Prune dangling volumes & images."
+	Write-Host -ForegroundColor Green "	--pu			Prune unused volumes & images."
+	Write-Host -ForegroundColor Green "	--pc			Prune containers."
+	Write-Host -ForegroundColor Green "	--pa			Prune entire system."
 	Write-Host -ForegroundColor Green ""
 	Write-Host -ForegroundColor Green ""
 	Write-Host -ForegroundColor Green "Usage:"
-	Write-Host -ForegroundColor Green "				kubectl-env.ps1 [operation]"
+	Write-Host -ForegroundColor Green "				docker-env.ps1 [operation]"
 	Write-Host -ForegroundColor Green ""
 	Exit
 }
 
-If ($Operation -in ("--view", "--view-wide", "")) {
+If ($Operation -in ("--view", "")) {
 	Write-Host -ForegroundColor Green "=========================================================="
-	Write-Host -ForegroundColor Green "Current Azure Subscription"
-	Write-Host -ForegroundColor Green "=========================================================="
-	
-	az account show --query "name" --output tsv
-	
-	Write-Host ""
-    Write-Host -ForegroundColor Green "=========================================================="
-	Write-Host -ForegroundColor Green "Current Context"
+	Write-Host -ForegroundColor Green "System"
 	Write-Host -ForegroundColor Green "=========================================================="
 	
-	kubectl config get-contexts
+	docker system df
 	
 	Write-Host ""
 	Write-Host -ForegroundColor Green "=========================================================="
-	Write-Host -ForegroundColor Green "Namespaces"
+	Write-Host -ForegroundColor Green "All Images"
 	Write-Host -ForegroundColor Green "=========================================================="
 	
-	If ($Operation -in ("--view", "")) {
-		kubectl get namespaces
-	} ElseIf ($Operation -eq "--view-wide") {
-		kubectl get namespaces -o wide
-	}
+	docker images
+	
+	Write-Host ""
+	Write-Host -ForegroundColor Green "=========================================================="
+	Write-Host -ForegroundColor Green "All Containers"
+	Write-Host -ForegroundColor Green "=========================================================="
+	
+	docker ps -a
+	
+	Write-Host ""
+	Write-Host -ForegroundColor Green "=========================================================="
+	Write-Host -ForegroundColor Green "All Volumes"
+	Write-Host -ForegroundColor Green "=========================================================="
+	
+	docker volume ls
+	
+	Write-Host ""
+	Write-Host -ForegroundColor Red "=========================================================="
+	Write-Host -ForegroundColor Red "Dangling Images"
+	Write-Host -ForegroundColor Red "=========================================================="
+	
+	docker images -f dangling=true
+	
+	Write-Host ""
+	Write-Host -ForegroundColor Red "=========================================================="
+	Write-Host -ForegroundColor Red "Dangling Volumes"
+	Write-Host -ForegroundColor Red "=========================================================="
+	
+	docker volume ls -f dangling=true
+	
+	Write-Host ""
+} ElseIf ($Operation -eq "--reset") {
+	Write-Host -ForegroundColor Red "=========================================================="
+	Write-Host -ForegroundColor Red "Resetting Docker Networking Adapter"
+	Write-Host -ForegroundColor Red "=========================================================="
+	
+	Set-NetConnectionProfile -interfacealias "vEthernet (DockerNAT)" -NetworkCategory Private
 
 	Write-Host ""
-	Write-Host -ForegroundColor Green "=========================================================="
-	Write-Host -ForegroundColor Green "Releases"
-	Write-Host -ForegroundColor Green "=========================================================="
+} ElseIf ($Operation -eq "--pd") {
+	Write-Host -ForegroundColor Red "=========================================================="
+	Write-Host -ForegroundColor Red "Removing Dangling Volumes"
+	Write-Host -ForegroundColor Red "=========================================================="
 	
-	helm list
+	docker volume rm -f $(docker volume ls -qf dangling=true)
+	
+	Write-Host -ForegroundColor Red "=========================================================="
+	Write-Host -ForegroundColor Red "Removing Dangling Images"
+	Write-Host -ForegroundColor Red "=========================================================="
+	
+	docker rmi -f $(docker images -qf dangling=true)
 
 	Write-Host ""
-	Write-Host -ForegroundColor Green "=========================================================="
-	Write-Host -ForegroundColor Green "Ingress"
-	Write-Host -ForegroundColor Green "=========================================================="
+} ElseIf ($Operation -eq "--pu") {
+	Write-Host -ForegroundColor Red "=========================================================="
+	Write-Host -ForegroundColor Red "Removing Unused Volumes"
+	Write-Host -ForegroundColor Red "=========================================================="
 	
-	If ($Operation -in ("--view", "")) {
-		kubectl get ingress --all-namespaces
-	} ElseIf ($Operation -eq "--view-wide") {
-		kubectl get ingress --all-namespaces -o wide
-	}
+	docker volume prune -f
 
 	Write-Host ""
-	Write-Host -ForegroundColor Green "=========================================================="
-	Write-Host -ForegroundColor Green "Certificates"
-	Write-Host -ForegroundColor Green "=========================================================="
+	Write-Host -ForegroundColor Red "=========================================================="
+	Write-Host -ForegroundColor Red "Removing Unused Images"
+	Write-Host -ForegroundColor Red "=========================================================="
 	
-	If ($Operation -in ("--view", "")) {
-		kubectl get certificates --all-namespaces
-	} ElseIf ($Operation -eq "--view-wide") {
-		kubectl get certificates --all-namespaces -o wide
-	}
+	docker image prune -af
 
 	Write-Host ""
-	Write-Host -ForegroundColor Green "=========================================================="
-	Write-Host -ForegroundColor Green "Cluster Issuers"
-	Write-Host -ForegroundColor Green "=========================================================="
+} ElseIf ($Operation -eq "--pc") {
+	Write-Host -ForegroundColor Red "=========================================================="
+	Write-Host -ForegroundColor Red "Removing All Containers"
+	Write-Host -ForegroundColor Red "=========================================================="
 	
-	If ($Operation -in ("--view", "")) {
-		kubectl get clusterissuers --all-namespaces
-	} ElseIf ($Operation -eq "--view-wide") {
-		kubectl get clusterissuers --all-namespaces -o wide
-	}
+	docker rm -fv $(docker ps -aq)
 
 	Write-Host ""
-	Write-Host -ForegroundColor Green "=========================================================="
-	Write-Host -ForegroundColor Green "Deployments"
-	Write-Host -ForegroundColor Green "=========================================================="
+} ElseIf ($Operation -eq "--pa") {
+	Write-Host -ForegroundColor Red "=========================================================="
+	Write-Host -ForegroundColor Red "Removing All Containers"
+	Write-Host -ForegroundColor Red "=========================================================="
 	
-	If ($Operation -in ("--view", "")) {
-		kubectl get deployments --all-namespaces
-	} ElseIf ($Operation -eq "--view-wide") {
-		kubectl get deployments --all-namespaces -o wide
-	}
-	
-	Write-Host ""
-	Write-Host -ForegroundColor Green "=========================================================="
-	Write-Host -ForegroundColor Green "Replica Sets"
-	Write-Host -ForegroundColor Green "=========================================================="
-	
-	If ($Operation -in ("--view", "")) {
-		kubectl get rs --all-namespaces
-	} ElseIf ($Operation -eq "--view-wide") {
-		kubectl get rs --all-namespaces -o wide
-	}
+	docker rm -fv $(docker ps -aq)
 
-	Write-Host ""
-	Write-Host -ForegroundColor Green "=========================================================="
-	Write-Host -ForegroundColor Green "Replication Controllers"
-	Write-Host -ForegroundColor Green "=========================================================="
+	Write-Host -ForegroundColor Red "=========================================================="
+	Write-Host -ForegroundColor Red "Removing All Images"
+	Write-Host -ForegroundColor Red "=========================================================="
 	
-	If ($Operation -in ("--view", "")) {
-		kubectl get rc --all-namespaces
-	} ElseIf ($Operation -eq "--view-wide") {
-		kubectl get rc --all-namespaces -o wide
-	}
+	docker rmi $(docker images -qf dangling=true)
+	docker rmi $(docker images -q)
+	docker image prune -af
+
+	Write-Host -ForegroundColor Red "=========================================================="
+	Write-Host -ForegroundColor Red "Removing All Volumes"
+	Write-Host -ForegroundColor Red "=========================================================="
 	
-	Write-Host ""
-	Write-Host -ForegroundColor Green "=========================================================="
-	Write-Host -ForegroundColor Green "Services"
-	Write-Host -ForegroundColor Green "=========================================================="
-	
-	If ($Operation -in ("--view", "")) {
-		kubectl get svc --all-namespaces
-	} ElseIf ($Operation -eq "--view-wide") {
-		kubectl get svc --all-namespaces -o wide
-	}
-	
-	Write-Host ""
-	Write-Host -ForegroundColor Green "=========================================================="
-	Write-Host -ForegroundColor Green "Pods"
-	Write-Host -ForegroundColor Green "=========================================================="
-	
-	If ($Operation -in ("--view", "")) {
-		kubectl get pods --all-namespaces
-	} ElseIf ($Operation -eq "--view-wide") {
-		kubectl get pods --all-namespaces -o wide
-	}
-	
+	docker volume rm (docker volume ls -qf dangling=true)
+	docker volume rm $(docker volume ls)
+	docker volume prune -f
+
 	Write-Host ""
 } Else {
 	Write-Host -ForegroundColor Red "=========================================================="
 	Write-Host -ForegroundColor Red "Wrong Operation Received"
-	Write-Host -ForegroundColor Red "Use: --view | --view-wide"
+	Write-Host -ForegroundColor Red "Use: --view | --reset | --pd | --pu | --pc | --pa"
 	Write-Host -ForegroundColor Red "=========================================================="
 }
