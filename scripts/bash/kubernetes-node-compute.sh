fbffc4bab3d1799bc483f77ef2de2bfc822e2bb8
marcinnarlochbd <37069983+marcinnarlochbd@users.noreply.github.com>
2019-07-15 15:49:07 +0100

    Update kubernetes-node-compute.sh

--- scripts/bash/kubernetes-node-compute.sh
+++ scripts/bash/kubernetes-node-compute.sh
@@ -1,1 +1,9 @@
+echo "***********************************************************************"
+echo "Node Compute"
+echo "***********************************************************************"
 kubectl get nodes --no-headers | awk '{print $1}' | xargs -I {} sh -c 'echo {}; kubectl describe node {} | grep Allocated -A 5 | grep -ve Event -ve Allocated -ve percent -ve -- ; echo'
+
+echo "***********************************************************************"
+echo "Pod Compute"
+echo "***********************************************************************"
+kubectl top pod --all-namespaces
